d74f691e006dd13aa3145836e226c83fb73a58d1
Gaurav10997 <avdhika@gmail.com>
2023-10-06 16:52:05 +0530

    chat box completed

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,27 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react'
 import "./Chat.css"
-// import  * as tfjs from "@tensorflow/tfjs"
-// import * as qna from "@tensorflow-models/qna"
 import Message from './message/Message'
 import ChatHeader from './chatHeader/ChatHeader'
-import ChatInput from "./chatinput/ChatInput"
+import ThreeDotsHelper from './common/ThreeDotsHelper'
+
 
 function Chat({model}) {
-  // const [model ,setModel] = useState(null)
   const [response,setResonse] = useState([]);
-  const [questions,setQuestions] = useState([]);
+  const [questions,setQuestions] = useState("");
   const [responses,setResponses] = useState([]);
   const [passage,setPassage] = useState("")
-  const inputRef = useRef();
-  // const [loadingChats , setLoadingChats] = useState(true)
+  const [pending,setPending] = useState(false)
 
-//setPassage(`${data.data.text}`)
+  const chatboxRef = useRef(null);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    // Scroll to the bottom of the chatbox when new messages are added
+    chatboxRef.current.scrollTop = chatboxRef.current.scrollHeight;
+  }, [responses]);
+
+
+
   useEffect(()=>{ 
     fetch("http://localhost:8080/getYourPdfText")
     .then((res)=>res.json())
     .then((data)=>{
       return(
-        // console.log(data)
         setPassage(`${data.data}`)
       )
     })
@@ -30,34 +35,36 @@ function Chat({model}) {
 
 
 useEffect(()=>{
-  // console.log("main function called ")
-  AutomaticAnswers();
+  setTimeout(()=>{
+    AutomaticAnswers();
+  },2000)
+ 
 },[questions])
 
 
 async function AutomaticAnswers(){
     const question = inputRef.current.value
-    // const passage = `TensorFlow is an open-source machine learning framework developed by the Google Brain team. It's a highly flexible and scalable tool that facilitates the development and deployment of machine learning and deep learning models. TensorFlow is widely adopted in the artificial intelligence and machine learning communities due to its versatility and robustness. It supports a variety of machine learning tasks, including image and speech recognition, natural language processing, 
-    // and more. 
-    // TensorFlow offers high-level APIs like Keras, simplifying the process of building and training neural networks."`
-    console.log(passage)
     const answers = await model.findAnswers(question, passage);
     // console.log(answers)
-    const text = answers.length>0?answers[0].text:"Something Went Wrong Try Differnt Questions"
+    const text = answers.length>0?answers[0].text:"An error occurred; please ask a different question."
     const lastObject = responses.pop();
     const modifiedObject = {question:lastObject.question  , answer:text}
     const updatedResponse = [...responses,modifiedObject]
-  
+
     setResponses(updatedResponse)
     setResonse([...response,text])
     inputRef.current.value=""
+    setPending(false)
 }
+
 
 
 
 async function handleSubmitQuestion(e) {
   // Prevent the default form submission behavior
   e.preventDefault();
+  console.log("handle submit clicked")
+  setPending(true)
 
   // Make a copy of the current responses array
   const updatedResponses = [...responses];
@@ -66,21 +73,23 @@ async function handleSubmitQuestion(e) {
   const newQuestion = inputRef.current.value;
 
   // Update the responses state by adding a new response object
-  setResponses([...responses, { question: newQuestion }]);
+  console.log(newQuestion)
+  setResponses([...responses, { question: newQuestion  }]);
 
   // Update the questions state by adding the new question
-  setQuestions([...questions, newQuestion]);
+  setQuestions(Math.random());
+  // settempquestion(newQuestion)
+   
 
 }
- console.log(passage)
   return (
             <>
                 <>
-
+  
                 <section className="msger">
                   < ChatHeader/>
                   <div style={{margin:"10px"}}></div>
-                    <div className="msger-chat">
+                    <div className="msger-chat" ref={chatboxRef}>
                     <Message answer={"Hey , Now You can ask About Your Pdf "} ></Message>
                       {responses.map(({question,answer})=>{
                         return(
@@ -90,7 +99,9 @@ async function handleSubmitQuestion(e) {
                           </>
                         )
                       })}
+                       {pending && <ThreeDotsHelper/>}
                     </div>
+                   
                      
                       <div style={{margin:"10px"}}></div>
                       <div className="msger-inputarea" >
---
+++ client/src/components/common/ThreeDotsHelper.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ThreeDots } from 'react-loader-spinner'
+function ThreeDotsHelper() {
+  return (
+    <ThreeDots 
+height="80" 
+width="80" 
+radius="9"
+color="#579ffb" 
+ariaLabel="three-dots-loading"
+wrapperStyle={{}}
+wrapperClassName=""
+visible={true}
+ />
+  )
+}
+
+export default ThreeDotsHelper
