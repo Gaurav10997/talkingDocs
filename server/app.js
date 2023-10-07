d1e283cd5040d1477c49988250a9d04cf4b99e08
Gaurav10997 <avdhika@gmail.com>
2023-10-07 15:51:57 +0530

    mongodb included

--- server/app.js
+++ server/app.js
@@ -7,8 +7,7 @@ const cors = require("cors");
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/talkingdocs')
-.then(console.log("Mongodb Connection Succesfully"))
+
 
 
 
