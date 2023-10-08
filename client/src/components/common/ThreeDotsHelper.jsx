627583603fe56f5f87f9bc048b16de8a0ad3bbe9
Gaurav10997 <avdhika@gmail.com>
2023-10-08 15:24:28 +0530

    auth username bug

--- client/src/components/common/ThreeDotsHelper.jsx
+++ client/src/components/common/ThreeDotsHelper.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { ThreeDots } from 'react-loader-spinner'
 function ThreeDotsHelper() {
   return (
-    <ThreeDots 
-height="80" 
-width="80" 
-radius="9"
-color="#579ffb" 
-ariaLabel="three-dots-loading"
-wrapperStyle={{}}
-wrapperClassName=""
-visible={true}
- />
+        <ThreeDots 
+          height="80" 
+          width="80" 
+          radius="9"
+          color="#579ffb" 
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+    />
   )
 }
 
-export default ThreeDotsHelper
+export default ThreeDotsHelper;
